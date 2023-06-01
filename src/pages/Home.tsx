ec6a32d7fa8621ea1732ba4961f64e9456c4c915
utsavarora33@gmail.com <utsavarora33@gmail.com>
2023-06-01 05:47:50 +0530

    Portfolio App created. Home Page is added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Header from '../components/Header/Header'
+
+const Home = () => {
+  return (
+    <div><Navbar />
+    <Header />
+    </div>
+  )
+}
+
+export default Home
